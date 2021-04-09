e454fd8dcd7a60a61177a8a922932ee1a48917d1
Daniel Oselu <doselu3@gmail.com>
2021-04-09 14:12:53 +0300

    modified views to add Flight data

--- flights/views.py
+++ flights/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
+from .models import Flight
+
 # Create your views here.
 
 
 def index(request):
-    pass
+    return render(request, "flights/index.html", {
+        'flights' : Flight.objects.all(),
+    })
